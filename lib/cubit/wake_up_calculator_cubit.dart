c34ddd2d8de8e3f1ef9ea3d56eadd22cc7b08f34
Jakub Szczepaniak <j.szczepaniak02@gmail.com>
2021-10-19 21:32:22 +0200

    add updating data in bloc

--- lib/cubit/wake_up_calculator_cubit.dart
+++ lib/cubit/wake_up_calculator_cubit.dart
@@ -1,22 +1,63 @@
 import 'package:bloc/bloc.dart';
 import 'package:clock/clock.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 
 part 'wake_up_calculator_state.dart';
 
 class WakeUpCalculatorCubit extends Cubit<WakeUpCalculatorState> {
-  WakeUpCalculatorCubit({required this.clock}) : super(WakeUpCalculatorInitial());
+  WakeUpCalculatorCubit({required this.clock})
+      : super(WakeUpCalculatorInitial()) {
+    _time = clock.now();
+  }
 
   final Clock clock;
+  HourFormat _hourFormat = HourFormat.h12;
+  DateTime _time = DateTime.now();
+  CalculatingType _calculatingType = CalculatingType.GoToSleep;
+
+  void changeHourFormat(HourFormat newHourFormat) {
+    _hourFormat = newHourFormat;
+    _updateState();
+  }
+
+  void changeCalculatingType(CalculatingType newCalculatingType) {
+    _calculatingType = newCalculatingType;
+    _updateState();
+  }
+
+  void changeHour(DateTime newTime) {
+    _time = newTime;
+    _updateState();
+  }
+
+  void submit() {
+    // TODO: implement this
+    throw UnimplementedError();
+  }
+
+  List<DateTime> _calculateHour(DateTime time, bool? subtract){
+    List<DateTime> list = [];
+
+    for(int i=0; i<4; i++){
+      if(subtract ?? false){
+        list.add(time.subtract(Duration(minutes: 90*(3+i))));
+      }else{
+        list.add(time.add(Duration(minutes: 90*(3+i))));
+      }
+    }
+
+    return list;
+  }
+
+
+
+  void _updateState() {
+    WakeUpCalculatorUpdate newUpdateState =
+        WakeUpCalculatorUpdate(_hourFormat, _calculatingType, _time);
+    emit(newUpdateState);
+  }
 }
 
-enum HourFormat {
-  h24,
-  h12
-}
+enum HourFormat { h24, h12 }
 
-enum CalculatingType{
-  GoToSleep,
-  WakeUp
-}
+enum CalculatingType { GoToSleep, WakeUp }
--- test/cubit/test_wake_up_calculator_cubit.dart
+++ test/cubit/test_wake_up_calculator_cubit.dart
@@ -18,28 +18,25 @@ void main() {
 
     blocTest('update hour format',
       build: () => cubit,
-      act: (cubit) {
-        var changeHourFormat = cubit.changeHourFormat(HourFormat.h24);
-        return changeHourFormat;
-      },
+      act: (cubit) => (cubit as WakeUpCalculatorCubit).changeHourFormat(HourFormat.h24),
       expect: () => [WakeUpCalculatorUpdate(HourFormat.h24, CalculatingType.GoToSleep, DateTime(2021, 10, 10, 20, 30, 0))],
     );
 
     blocTest('update calculating type',
       build: () => cubit,
-      act: (cubit) => cubit.changeCalculatingType(CalculatingType.WakeUp),
+      act: (cubit) => (cubit as WakeUpCalculatorCubit).changeCalculatingType(CalculatingType.WakeUp),
       expect: () => [WakeUpCalculatorUpdate(HourFormat.h12, CalculatingType.WakeUp, DateTime(2021, 10, 10, 20, 30, 0))]
     );
 
     blocTest('update hour',
       build: () => cubit,
-      act: (cubit) => cubit.changeHour(DateTime(2021, 10, 10, 21, 0, 0)),
+      act: (cubit) => (cubit as WakeUpCalculatorCubit).changeHour(DateTime(2021, 10, 10, 21, 0, 0)),
       expect: () => [WakeUpCalculatorUpdate(HourFormat.h12, CalculatingType.GoToSleep, DateTime(2021, 10, 10, 21, 0, 0))]
     );
 
     blocTest('update hour format, calculating type and hour',
       build: () => cubit,
-      act: (cubit) => cubit..changeHourFormat(HourFormat.h12)..changeCalculatingType(CalculatingType.WakeUp)..changeHour(DateTime(2021, 10, 10, 21, 0, 0)),
+      act: (cubit) => (cubit as WakeUpCalculatorCubit)..changeHourFormat(HourFormat.h12)..changeCalculatingType(CalculatingType.WakeUp)..changeHour(DateTime(2021, 10, 10, 21, 0, 0)),
       expect: () => [
         WakeUpCalculatorUpdate(HourFormat.h12, CalculatingType.GoToSleep, DateTime(2021, 10, 10, 20, 30, 0)),
         WakeUpCalculatorUpdate(HourFormat.h12, CalculatingType.WakeUp, DateTime(2021, 10, 10, 20, 30, 0)),
@@ -49,13 +46,13 @@ void main() {
 
     blocTest('submit after init',
       build: () => cubit,
-      act: (cubit) => cubit.submit(),
+      act: (cubit) => (cubit as WakeUpCalculatorCubit).submit(),
       expect: () => [WakeUpCalculatorResult(["01:00", "02:30", "4:00", "5:30"], CalculatingType.GoToSleep)]
     );
 
     blocTest('submit 2 times',
         build: () => cubit,
-        act: (cubit) => cubit..submit()..submit(),
+        act: (cubit) => (cubit as WakeUpCalculatorCubit)..submit()..submit(),
         expect: () => [
           WakeUpCalculatorResult(["01:00", "02:30", "4:00", "5:30"], CalculatingType.GoToSleep),
           WakeUpCalculatorResult(["01:00", "02:30", "4:00", "5:30"], CalculatingType.GoToSleep)
@@ -64,7 +61,7 @@ void main() {
 
     blocTest("submit after init 12 hour format",
       build: () => cubit,
-      act: (cubit) => cubit..changeHourFormat(HourFormat.h12)..submit(),
+      act: (cubit) => (cubit as WakeUpCalculatorCubit)..changeHourFormat(HourFormat.h12)..submit(),
       expect: () => [
         WakeUpCalculatorUpdate(HourFormat.h12, CalculatingType.GoToSleep, DateTime(2021, 10, 10, 20, 30, 0)),
         WakeUpCalculatorResult(["01:00 AM", "02:30 AM", "4:00 AM", "5:30 AM"], CalculatingType.GoToSleep)
@@ -73,7 +70,7 @@ void main() {
 
     blocTest('submit after init with WakeUp type',
       build: () => cubit,
-      act: (cubit) => cubit..changeCalculatingType(CalculatingType.WakeUp)..submit(),
+      act: (cubit) => (cubit as WakeUpCalculatorCubit)..changeCalculatingType(CalculatingType.WakeUp)..submit(),
       expect: () => [
         WakeUpCalculatorUpdate(HourFormat.h24, CalculatingType.WakeUp, DateTime(2021, 10, 10, 20, 30, 0)),
         WakeUpCalculatorResult(["11:30", "13:00", "14:30", "16:00"], CalculatingType.WakeUp)
@@ -82,7 +79,7 @@ void main() {
 
     blocTest('submit after init with WakeUp type and 12 hour format',
         build: () => cubit,
-        act: (cubit) => cubit..changeCalculatingType(CalculatingType.WakeUp)..changeHourFormat(HourFormat.h12)..submit(),
+        act: (cubit) => (cubit as WakeUpCalculatorCubit)..changeCalculatingType(CalculatingType.WakeUp)..changeHourFormat(HourFormat.h12)..submit(),
         expect: () => [
           WakeUpCalculatorUpdate(HourFormat.h24, CalculatingType.WakeUp, DateTime(2021, 10, 10, 20, 30, 0)),
           WakeUpCalculatorUpdate(HourFormat.h12, CalculatingType.WakeUp, DateTime(2021, 10, 10, 20, 30, 0)),
@@ -92,7 +89,7 @@ void main() {
 
     blocTest('submit second time after change inputs',
       build: () => cubit,
-      act: (cubit) => cubit..submit()..changeHourFormat(HourFormat.h12)..changeCalculatingType(CalculatingType.WakeUp)..changeHour(DateTime(2021, 10, 10, 21, 0, 0))..submit(),
+      act: (cubit) => (cubit as WakeUpCalculatorCubit)..submit()..changeHourFormat(HourFormat.h12)..changeCalculatingType(CalculatingType.WakeUp)..changeHour(DateTime(2021, 10, 10, 21, 0, 0))..submit(),
       expect: () => [
         WakeUpCalculatorResult(["01:00", "02:30", "4:00", "5:30"], CalculatingType.GoToSleep),
         WakeUpCalculatorUpdate(HourFormat.h12, CalculatingType.GoToSleep, DateTime(2021, 10, 10, 20, 30, 0)),
@@ -104,7 +101,7 @@ void main() {
 
     blocTest('update data before submit, update data after submit and submit again',
         build: () => cubit,
-        act: (cubit) => cubit..changeHourFormat(HourFormat.h24)..changeHour(DateTime(2021, 10, 10, 16, 0, 0))..submit()..changeHourFormat(HourFormat.h12)..changeCalculatingType(CalculatingType.WakeUp)..changeHour(DateTime(2021, 10, 10, 21, 0, 0))..submit(),
+        act: (cubit) => (cubit as WakeUpCalculatorCubit)..changeHourFormat(HourFormat.h24)..changeHour(DateTime(2021, 10, 10, 16, 0, 0))..submit()..changeHourFormat(HourFormat.h12)..changeCalculatingType(CalculatingType.WakeUp)..changeHour(DateTime(2021, 10, 10, 21, 0, 0))..submit(),
         expect: () => [
           WakeUpCalculatorUpdate(HourFormat.h24, CalculatingType.GoToSleep, DateTime(2021, 10, 10, 20, 30, 0)),
           WakeUpCalculatorUpdate(HourFormat.h24, CalculatingType.GoToSleep, DateTime(2021, 10, 10, 16, 0, 0)),
